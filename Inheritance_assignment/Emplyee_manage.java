718344059c4f49d03cfd83ed8e05e174e559bb0d
rachittheguru <rachitsharmakkr@gmail.com>
2025-10-28 14:10:33 +0530

    Inheritance assignment

---
+++ Inheritance_assignment/Emplyee_manage.java
@@ -0,0 +1,58 @@
+class Employee {
+    String name;
+    int id;
+    double salary;
+
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    void displayDetails() {
+        System.out.println("Name: " + name + ", ID: " + id + ", Salary: " + salary);
+    }
+}
+
+class Manager extends Employee {
+    int teamSize;
+
+    Manager(String name, int id, double salary, int teamSize) {
+        super(name, id, salary);
+        this.teamSize = teamSize;
+    }
+
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Team Size: " + teamSize);
+    }
+}
+
+class Developer extends Employee {
+    String programmingLanguage;
+
+    Developer(String name, int id, double salary, String programmingLanguage) {
+        super(name, id, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Programming Language: " + programmingLanguage);
+    }
+}
+
+class Intern extends Employee {
+    String duration;
+
+    Intern(String name, int id, double salary, String duration) {
+        super(name, id, salary);
+        this.duration = duration;
+    }
+
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Internship Duration: " + duration);
+    }
+}
---
+++ Inheritance_assignment/transport.java
@@ -0,0 +1,55 @@
+class Vehicle {
+    int maxSpeed;
+    String fuelType;
+
+    Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    void displayInfo() {
+        System.out.println("Max Speed: " + maxSpeed + " | Fuel Type: " + fuelType);
+    }
+}
+
+class Car extends Vehicle {
+    int seatCapacity;
+
+    Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Seat Capacity: " + seatCapacity);
+    }
+}
+
+class Truck extends Vehicle {
+    int loadCapacity;
+
+    Truck(int maxSpeed, String fuelType, int loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+class Motorcycle extends Vehicle {
+    boolean hasCarrier;
+
+    Motorcycle(int maxSpeed, String fuelType, boolean hasCarrier) {
+        super(maxSpeed, fuelType);
+        this.hasCarrier = hasCarrier;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Carrier: " + hasCarrier);
+    }
+}
